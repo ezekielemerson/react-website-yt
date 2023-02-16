447718bfa44c88898f18ea09a1a9b2b40bcfd562
ezekielemerson <eemerson2325@gmail.com>
2023-02-15 22:28:04 -0500

    pedro video2 first try

--- server/index.js
+++ server/index.js
@@ -1,9 +1,45 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "labelbox",
+  database: "CRUD-database",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get("/api/get", (req, res) => {
+//   const sqlSelect = "SELECT * FROM movie_reviews";
+//   db.query(sqlSelect, (err, result) => {
+//     console.log(result);
+//   });
+// });
 
 app.get("/", (req, res) => {
-  res.send("hello pedro");
+  const sqlInsert =
+    "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('bee movie', 'buzz');";
+  db.query(sqlInsert, (err, result) => {
+    res.send("hello pedro");
+  });
 });
+
+// app.post("/api/insert", (req, res) => {
+//   const movieName = req.body.movieName;
+//   const movieReview = req.body.movieReview;
+
+//   const sqlInsert =
+//     "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)";
+//   db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+//     console.log(err);
+//   });
+// });
 
 app.listen(3001, () => {
   console.log("running on port 3001");
